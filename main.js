f46f7f5ba39fc9765f1e05b22e2669496a4d4038
iyamujoshua <joshamu20@gmail.com>
2023-08-07 22:19:08 +0100

    api

---
+++ main.js
@@ -0,0 +1,29 @@
+// fetch('https://reqres.in/api/users',{
+//     method: 'POST',
+//     headers:{
+//        "Content-Type": "application/json"
+//     },
+//     body:JSON.stringify({
+//         name:'iyamuaise'
+//     })
+// })
+// .then(res => {
+//     return res.json()
+// })
+// .then(data => console.log(data))
+// .catch(error => console.log("error"))
+
+
+
+
+// fetch('https://jsonplaceholder.typicode.com/users')
+// .then(res => {return res.json();
+// })
+// .then(data =>{
+//      data.forEach(user => {
+//         const markup= `<li>${user.name}</li>`;
+//         document.querySelector('ul').insertAdjacentHTML('beforeend', markup)
+//      });
+//     })
+// .catch(error => console.log(error))
+
